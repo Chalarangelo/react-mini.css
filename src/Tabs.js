86ef00d7d89b43f7f8a07d7836491e2a5aa6aa8a
Angelos Chalaris <chalarangelo@gmail.com>
2017-05-30 10:43:40 +0300

    Updated Tabs component
    Mostly complete Tabs component, some additional things and polish need to be added, but overall it's working entirely fine.

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,75 +1,76 @@
 import React from 'react';
 import generateUniqueId from './util/idGenerator';
 
-
-/*
-<input type="radio" name="tab-group" id="tab1" checked aria-hidden="true">
-  <label for="tab1" aria-hidden="true">Tab 1</label>
-  <div>
-    <h3>Tab 1</h3>
-    <p>This is the first tab's content.</p>
-  </div>
-	*/
+class Tab extends React.Component {
+	render() {
+		return React.createElement('div',{},this.props.children);
+	}
+}
 
 export default class Tabs extends React.Component {
 	constructor(props) {
 		super(props);
 		var state = {
 			group: 'tab_group_' + generateUniqueId(),
-			tabs : []
+			children : []
 		}
-		this.props.tabs.forEach(function(propTab){
-			var tab = propTab;
-			tab.id = 'tab_' + generateUniqueId();
-			state.tabs.push(tab);
-		});
+		if (!Array.isArray(this.props.children))
+			var tempProps = [this.props.children];
+		else
+			var tempProps = this.props.children;
+		tempProps.forEach(
+			function(child){
+				if (!child instanceof Tab){
+					 throw "All children of a 'Tabs' component need to be of type 'Tab'. Expected type: 'Tab' Found type: '"+child.class+"'";
+					 return;
+				}
+				var tab = Object.assign({}, child);
+				tab.internalId = 'tab_' + generateUniqueId();
+				state.children.push(tab);
+			}
+		);
 		this.state = state;
 	}
 
-	render() {
-			// return (
-			// 	<div className="tabs">
-			//
-			// 	</div>
-			// );
-			var kids = [];
-			var group = this.state.group;
-			this.state.tabs.forEach(function(tab){
-				kids.push(React.createElement('input',{type: 'radio', name : group, id : tab.id, defaultChecked : true, 'aria-hidden': true}));
-				kids.push(React.createElement('label',{htmlFor : tab.id, 'aria-hidden': true},tab.labelText));
-				kids.push(React.createElement('div',{},tab.children));
-			});
-			return React.createElement('div',{className : 'tabs'},kids);
+	render(){
+		var childrenToRender = [];
+		var groupName = this.state.group;
+		var hasChecked = false;
+		this.state.children.forEach(function(tab){
+			if (tab.props.checked) {
+				hasChecked = true;
+			}
+			childrenToRender.push(
+				React.createElement(
+					'input',{
+						type: 'radio',
+						name : groupName,
+						id : tab.internalId,
+						defaultChecked : (tab.props.checked) ? true : false,
+						'aria-hidden': 'true'}
+				)
+			);
+			childrenToRender.push(
+				React.createElement(
+					'label',{
+						'htmlFor' : tab.internalId,
+						'aria-hidden' : 'true'
+					},
+					'demo-tab'+tab.internalId
+				)
+			);
+			childrenToRender.push(React.createElement('div',{},tab.props.children));
+		});
+		if (!hasChecked)
+			childrenToRender[0] = React.createElement(
+				'input', {
+					type: 'radio',
+					name : groupName,
+					id : this.state.children[0].internalId,
+					defaultChecked : true,
+					'aria-hidden':'true'
+				}
+			);
+		return React.createElement('div',{'className' : 'tabs'},childrenToRender);
 	}
-	// this.state.tabs.forEach(function(tab){
-	// 	<input type="radio" name={this.state.group} id={tab.id} defaultChecked aria-hidden="true" />
-	// 	<label htmlFor={tab.id} aria-hidden="true">{tab.labelText}</label>
-	// 	<div>
-	// 		{tab.children}
-	// 	</div>
-	// }
-	// render () {
-	// 	return (
-	// 		<div className="tabs">
-	// 		  <input type="radio" name="tab-group" id="tab1" defaultChecked aria-hidden="true"/>
-	// 		  <label htmlFor="tab1" aria-hidden="true">Tab 1</label>
-	// 		  <div>
-	// 		    <h3>Tab 1</h3>
-	// 		    <p>This is the first tab's content.</p>
-	// 		  </div>
-	// 		  <input type="radio" name="tab-group" id="tab2" aria-hidden="true"/>
-	// 		  <label htmlFor="tab2" aria-hidden="true">Tab 2</label>
-	// 		  <div>
-	// 		    <h3>Tab 2</h3>
-	// 		    <p>This is the second tab's content.</p>
-	// 		  </div>
-	// 		  <input type="radio" name="tab-group" id="tab3" aria-hidden="true"/>
-	// 		  <label htmlFor="tab3" aria-hidden="true">Tab 3</label>
-	// 		  <div>
-	// 		    <h3>Tab 3</h3>
-	// 		    <p>This is the third tab's content.</p>
-	// 		  </div>
-	// 		</div>
-	// 	);
-	// }
 }

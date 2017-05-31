a06ad362d72fa6f2b3a1abfad40097f56700903c
Angelos Chalaris <chalarangelo@gmail.com>
2017-05-31 10:55:25 +0300

    Updated Tabs to functional component
    Refactored the first component to be a stateless one, as these are all presentational components. More changes will be made to the rest of them.

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,99 +1,94 @@
 import React from 'react';
 import generateUniqueId from './util/idGenerator';
 
-module.exports = {
-	// Module constants (change according to your flavor file)
-	const TABS_CLASS_NAME = 'tabs';
-	const TABS_STACKED_CLASS_NAME = 'stacked';
-	// Technically speaking, the Tab is a non-component, meaning it should not be
-	// rendered on its own. It depends on the Tabs component for proper rendering.
-	class Tab extends React.Component {
-		render() {
-			var tabProps = Object.assign({}, this.props);
-			delete tabProps.tabTitle;
-			return React.createElement('div', this.props, this.props.children);
+// Module constants (change according to your flavor file)
+var tabsClassName = 'tabs';
+var tabsStackedClassName = 'stacked';
+
+// Technically speaking, the Tab is a non-component, meaning it should not be
+// rendered on its own. It depends on the Tabs component for proper rendering.
+function Tab(props){
+	var outProps = Object.assign({}, props);
+	if (typeof outProps.checked === 'undefined') outProps.checked = false;
+	if (typeof outProps.defaultChecked === 'undefined') outProps.defaultChecked = false;
+	if (typeof outProps.tabTitle === 'undefined') throw "Error: The 'tabTitle' property of the 'Tab' component is mandatory";
+	delete outProps.tabTitle;
+	return React.createElement(
+		'div', outProps, outProps.children
+	);
+}
+
+// Tabs component.
+function Tabs(props){
+	var outProps = Object.assign({}, props);
+	if (typeof outProps.stacked === 'undefined') outProps.stacked = false;
+	if (typeof outProps.tabbingType === 'undefined') outProps.tabbingType = 'radio';
+	var group = 'tab_group_'+ generateUniqueId();
+	var children = [];
+	var temp = (Array.isArray(props.children)) ? props.children : [props.children];
+	temp.forEach(
+		function(child) {
+			if (!child instanceof Tab) throw "Error: All children of a 'Tabs' component must be 'Tab' components.";
+			var tab = Object.assign({}, child);
+			tab.internalId = 'tab_' + generateUniqueId();
+			children.push(tab);
 		}
-	};
-	// Default props for the Tab component.
-	Tab.defaultProps = {
-		checked : false,
-		defaultChecked : false,
-		tabTitle : 'Tab'
-	};
-	// Tabs component.
-	class Tabs extends React.Component {
-		// The list of children is verified and passed into the component's state.
-		constructor(props) {
-			super(props);
-			var state = {
-				group: 'tab_group_' + generateUniqueId(),
-				children : []
-			}
-			var tempProps = (Array.isArray(this.props.children)) ? this.props.children : [this.props.children];
-			tempProps.forEach(
-				function(child){
-					if (!child instanceof Tab){
-						 throw "All children of a 'Tabs' component need to be of type 'Tab'. Expected type: 'Tab' Found type: '"+child.class+"'";
-						 return;
-					}
-					var tab = Object.assign({}, child);
-					tab.internalId = 'tab_' + generateUniqueId();
-					state.children.push(tab);
-				}
+	);
+	var childrenToRender = [];
+	var tabbingType = (props.tabbingType == 'checkbox' && props.stacked) ? 'checkbox' : 'radio';
+	var hasChecked = false;
+	children.forEach(
+		function(child) {
+			if (child.props.checked || child.props.defaultChecked) hasChecked = true;
+			childrenToRender.push(
+				React.createElement(
+					'input', {
+						type: tabbingType,
+						name : group,
+						key : child.internalId+'_input',
+						id : child.internalId,
+						defaultChecked : (child.props.checked || child.props.defaultChecked) ? true : false,
+						'aria-hidden': 'true'}
+					)
 			);
-			this.state = state;
+			childrenToRender.push(
+				React.createElement(
+					'label', {
+						'htmlFor' : child.internalId,
+						key : child.internalId+'_label',
+						'aria-hidden' : 'true'
+					}, child.props.tabTitle
+				)
+			);
+			var childProps = Object.assign({}, child.props);
+			childProps.key = child.internalId+'_content';
+			delete childProps.tabTitle;
+			childrenToRender.push(
+				React.createElement(
+					'div', childProps, childProps.children
+				)
+			);
 		}
-		// Rendering consumes the required props and children from state to
-		// generate the tabs.
-		render(){
-			var childrenToRender = [];
-			var groupName = this.state.group;
-			var tabbingType = (this.props.tabbingType == 'checkbox' && this.props.stacked) ? 'checkbox' : 'radio';
-			var hasChecked = false;
-			this.state.children.forEach(function(tab) {
-				if (tab.props.checked || tab.props.defaultChecked) hasChecked = true;
-				childrenToRender.push(
-					React.createElement('input', {
-							type: tabbingType,
-							name : groupName,
-							key : tab.internalId+'_input',
-							id : tab.internalId,
-							defaultChecked : (tab.props.checked || tab.props.defaultChecked) ? true : false,
-							'aria-hidden': 'true'})
-				);
-				childrenToRender.push(
-					React.createElement('label', {
-							'htmlFor' : tab.internalId,
-							key : tab.internalId+'_label',
-							'aria-hidden' : 'true'
-						}, tab.props.tabTitle)
-				);
-				var tabProps = Object.assign({}, tab.props);
-				tabProps.key = tab.internalId+'_content';
-				delete tabProps.tabTitle;
-				childrenToRender.push(React.createElement('div',tabProps,tab.props.children));
-			});
-			if (!hasChecked)
-				childrenToRender[0] = React.createElement('input', {
-					type: tabbingType,
-					name : groupName,
-					id : this.state.children[0].internalId,
-					defaultChecked : true,
-					'aria-hidden':'true'
-				});
-			var tabsProps = Object.assign({}, this.props);
-			if (typeof tabsProps.className === 'undefined')
-				tabsProps.className = (this.props.stacked) ? (TABS_CLASS_NAME + ' ' + TABS_STACKED_CLASS_NAME) : TABS_CLASS_NAME;
-			else
-				tabsProps.className += (this.props.stacked) ? (' ' + TABS_CLASS_NAME + ' ' + TABS_STACKED_CLASS_NAME) : TABS_CLASS_NAME;
-			delete tabsProps.stacked;
-			delete tabsProps.tabbingType;
-			return React.createElement('div',tabsProps,childrenToRender);
-		}
-		// Default props for the Tabs component.
-		Tabs.defaultProps = {
-			stacked : false,
-			tabbingType	: 'radio'
-		};
-	}
+	);
+	if (!hasChecked)
+		childrenToRender[0] = React.createElement(
+			'input', {
+				type: tabbingType,
+				name : group,
+				key : children[0].internalId,
+				id : children[0].internalId,
+				defaultChecked : true,
+				'aria-hidden':'true'
+			}
+		);
+	var outProps = Object.assign({}, props);
+	if (typeof outProps.className === 'undefined')
+		outProps.className = (props.stacked) ? (tabsClassName + ' ' + tabsStackedClassName) : tabsClassName;
+	else
+		outProps.className += (props.stacked) ? (' ' + tabsClassName + ' ' + tabsStackedClassName) : tabsClassName;
+	delete outProps.stacked;
+	delete outProps.tabbingType;
+	return React.createElement(
+		'div',outProps,childrenToRender
+	);
 }
